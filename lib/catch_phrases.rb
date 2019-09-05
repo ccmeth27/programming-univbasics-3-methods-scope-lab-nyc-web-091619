1f02d152fcfd712dcc1f3247fed909ba5aaaec87
Connor Meth <connormeth27@gmail.com>
2019-09-05 19:11:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,10 +3,10 @@ def phrase
 end
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
